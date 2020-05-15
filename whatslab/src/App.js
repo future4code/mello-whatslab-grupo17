ed43ae13f546eb4affdf23f3c91050c7a67bf9df
Diogo Gaspar <ghostlabz@gmail.com>
2020-05-15 14:17:38 -0300

    Form component começando a funcionar

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form"
 
 function App() {
   return (
     <div className="App">
       <div>
-      olá
       </div>
       <footer>
-        <form>
+        <Form
+
+        />
           
-        </form>
       </footer>
 
     </div>
--- whatslab/src/components/Form.js
+++ whatslab/src/components/Form.js
@@ -1,18 +1,33 @@
 import React from "react";
-import styled from "styled-components"
+//import styled from "styled-components"
 
 class Form extends React.Component {
   state = {
-    message = {
+    message: {
       userName: "",
       userMessage: ""
     }
   }
     onClickSend = () => {
-      const newMessage = [...this.state.message]
+      const newMessage = {
+        userName:  this.state.userName,
+        userMessage: this.state.userMessage,
+      }
+      const messages = [newMessage, ...this.state.message ];
+
+      this.setState({ message: messages})
     }
   
-  render() {
 
+  render() {
+    return (
+    <div>
+      <input type="text" value={this.state.userName} placeholder={'User'} />
+      <input type="text" value={this.state.userMessage} placeholder={'Message'} />
+      <button onClick={this.onClickSend}>Enviar</button>
+    </div>
+    )
   }
 }
+
+export default Form;
