da8938144a684d70887d915b433ad7fbb1fe9cf9
Diogo Gaspar <ghostlabz@gmail.com>
2020-05-15 14:27:22 -0300

    Form component começando a funcionar

--- whatslab/src/components/Form.js
+++ whatslab/src/components/Form.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React from 'react';
 //import styled from "styled-components"
 
 class Form extends React.Component {
   state = {
     message: {
-      userName: "",
-      userMessage: ""
-    }
-  }
-    onClickSend = () => {
-      const newMessage = {
-        userName:  this.state.userName,
-        userMessage: this.state.userMessage,
-      }
-      const messages = [newMessage, ...this.state.message ];
+      userName: '',
+      userMessage: '',
+    },
+  };
+  onClickSend = () => {
+    const newMessage = {
+      userName: this.state.userName,
+      userMessage: this.state.userMessage,
+    };
+    const messages = [newMessage, ...this.state.message];
 
-      this.setState({ message: messages})
-    }
-  
+    this.setState({ message: messages });
+  };
 
   render() {
     return (
-    <div>
-      <input type="text" value={this.state.userName} placeholder={'User'} />
-      <input type="text" value={this.state.userMessage} placeholder={'Message'} />
-      <button onClick={this.onClickSend}>Enviar</button>
-    </div>
-    )
+      <div>
+        <input type="text" value={this.state.userName} placeholder={'User'} />
+        <input
+          type="text"
+          value={this.state.userMessage}
+          placeholder={'Message'}
+        />
+        <button onClick={this.onClickSend}>Enviar</button>
+      </div>
+    );
   }
 }
 
